171de9efd20645f49e4404a3704947506a1d9805
LuizBeraldi <luizhenrique.beraldi@gmail.com>
2024-12-24 09:36:07 -0300

    modified:   GeradorDeCodigo/ast.c

--- GeradorDeCodigo/ast.c
+++ GeradorDeCodigo/ast.c
@@ -1711,6 +1711,22 @@ ResultadoExpr *avaliarExpressao(Expressao *expressao, void **globalHash, void **
             regT = opeAritmeticas(tipoEsq, regEsq, tipoDir, regDir, "mul");
             resultado->numReg = regT;
             resultado->tipoReg = 0;
+
+            return resultado;
+        }else if(expressao->operador == DIVIDE){
+            if(esq->NoAuxid && ((HashNo *)esq->NoAuxid)->tipok == VECTOR){
+                regEsq = loadDoArray(esq->numReg);
+                tipoEsq = 0;
+            }
+
+            if(dir->NoAuxid && ((HashNo *)dir->NoAuxid)->tipok == VECTOR){
+                regDir = loadDoArray(dir->numReg);
+                tipoDir = 0;
+            }
+            resultado = criarResultadoExpressao(esq->tipoVar, esq->ptr, esq->atribuicao / dir->atribuicao);
+            regT = opDivisao(tipoEsq, regEsq, tipoDir, regDir, "mflo");
+            resultado->numReg = regT;
+            resultado->tipoReg = 0;
             
             return resultado;
         }
